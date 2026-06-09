61132a2124ab0cc8546b835248afea38dd25b6ce
Codelise <claycaluscos@gmail.com>
2026-06-09 09:21:50 +0800

    Refactored again

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, useCallback, use } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { createClient } from "../../../lib/supabase/client";
 import { useRouter } from "next/navigation";
 import {
